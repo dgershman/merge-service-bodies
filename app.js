1172c76951d685f57b5524600c84d09d4957472f
Danny Gershman <danny.gershman@gmail.com>
2018-11-14 23:56:04 -0500

    export to a csv

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 let fs = require('fs');
+var stringify = require('csv-stringify');
 const phone_numbers = JSON.parse(fs.readFileSync('phone_numbers.json', 'utf8'));
 const tomato = JSON.parse(fs.readFileSync('tomato.json', 'utf8'));
 let results = [];
@@ -12,8 +13,13 @@ for (let item of phone_numbers) {
         results.push(item)
     }
 }
-
-console.log(JSON.stringify(results))
+stringify(results, (err, output) => {
+    fs.writeFile('results.csv', output, 'utf8', err => {
+        if (err) {
+            console.log(err);
+        }
+    })
+});
 
 function getServiceBodyMatch(name) {
     for (let item of tomato) {
